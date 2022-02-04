61b20d7f45ded32dac23b6f73fde1a70b72582f4
pramods12345 <31625894+pramods12345@users.noreply.github.com>
2022-02-04 11:28:05 +0530

    Uploaded from bitbucket

---
+++ PRAMODS123-chattingapp-039a9e5f9cc6/ChattingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChattingApp
+//
+//  Created by PRAMOD S on 18/06/19.
+//  Copyright © 2019 PRAMOD S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
